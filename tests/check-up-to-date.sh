79692b5f0fb2f033b5677be3e4407761822fddb7
M.P. Korstanje <rien.korstanje@gmail.com>
2022-05-14 01:53:00 +0200

    Quite tests

--- tests/check-up-to-date.sh
+++ tests/check-up-to-date.sh
@@ -8,12 +8,12 @@ git commit --quiet --all --message "Use the real polyglot-release script"
 
 # Create a fake release, without using polyglot-release to limit the log info
 git tag "v0.0.2"
-git push --tags
+git push --quiet --tags
 
 # Set the local script to an outdated version
 sed -i "s%^POLYGLOT_RELEASE_VERSION=.*$%POLYGLOT_RELEASE_VERSION=0.0.1%" polyglot-release
 git commit --quiet --all --message "Set POLYGLOT_RELEASE_VERSION to old version"
-git push
+git push --quiet
 
 # Try to make a self release
 ./polyglot-release 2.0.0
