1c5e7d5cdf3c63fdbe384e8981900d9a24a283a4
M.P. Korstanje <rien.korstanje@gmail.com>
2022-05-14 01:38:22 +0200

    Test against test remote

--- tests/check-up-to-date.sh
+++ tests/check-up-to-date.sh
@@ -4,7 +4,8 @@ set -e
 # Get the real script and set the release version to outdated version 0.0.0
 cp "$(which polyglot-release)" polyglot-release
 chmod 744 ./polyglot-release
-sed -i "s/^POLYGLOT_RELEASE_VERSION=.*$/POLYGLOT_RELEASE_VERSION=0.0.0/" polyglot-release
+sed -i "s%^POLYGLOT_RELEASE_VERSION=.*$%POLYGLOT_RELEASE_VERSION=0.0.0%" polyglot-release
+sed -i "s%^POLYGLOT_RELEASE_GIT_REPO=.*$%POLYGLOT_RELEASE_GIT_REPO=$(git config --get remote.origin.url)%" polyglot-release
 git commit --quiet --all --message "Set POLYGLOT_RELEASE_VERSION to old version"
 
 # Try self release
