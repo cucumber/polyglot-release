0d5117d2f63bab214ad5116c7a4e3b4d875e217b
M.P. Korstanje <rien.korstanje@gmail.com>
2022-05-21 23:58:13 +0200

    Add stub for remote polyglot release repo

--- tests/check-up-to-date.sh
+++ tests/check-up-to-date.sh
@@ -1,20 +1,18 @@
 # fixture: polyglot-release
 set -e
 
+# Copy the polyglot-release script amd set the version to v0.0.1
 pushd .. > /dev/null
-
-# Copy the polyglot-release script
 cp "$(which polyglot-release)" polyglot-release
 chmod 744 ./polyglot-release
-
-# Set the version to v0.0.1
 sed -i "s/^POLYGLOT_RELEASE_VERSION=.*$/POLYGLOT_RELEASE_VERSION=0.0.1/" polyglot-release
 popd > /dev/null
 
 # Create a release for v0.0.2 in git.
 # This makes the copy of polyglot-release out of date.
+pushd ../polyglot-release.git > /dev/null
 git tag "v0.0.2"
-git push --quiet --tags
+popd > /dev/null
 
 # Try to make a self release
 ./../polyglot-release 2.0.0
