f35d0272c75e93a42872ee425660ae0b8592603d
M.P. Korstanje <mpkorstanje@users.noreply.github.com>
2022-05-22 12:55:13 +0200

    Use polyglot-release to release polyglot-release (#65)

---
+++ tests/check-up-to-date.sh
@@ -0,0 +1,19 @@
+# fixture: ruby
+set -e
+
+# Copy the polyglot-release script and set the version to v0.0.1.
+# This simulates a user who has installed a released version of polyglot-release.
+pushd .. > /dev/null
+cp "$(which polyglot-release)" polyglot-release
+chmod 744 ./polyglot-release
+sed -i "s/^POLYGLOT_RELEASE_VERSION=.*$/POLYGLOT_RELEASE_VERSION=0.0.1/" polyglot-release
+popd > /dev/null
+
+# Create a release for v0.0.2 in polyglot-release.git.
+# This makes the installed version of polyglot-release out of date.
+pushd ../polyglot-release.git > /dev/null
+git tag "v0.0.2"
+popd > /dev/null
+
+# Try to make a release using the now out of date version.
+./../polyglot-release 2.0.0
---
+++ tests/created-by-contains-version.sh
@@ -0,0 +1,16 @@
+# fixture: ruby
+set -e
+
+pushd .. > /dev/null
+
+# Get the real script
+cp "$(which polyglot-release)" polyglot-release
+chmod 744 ./polyglot-release
+# Set the real script to a released version
+sed -i "s%^POLYGLOT_RELEASE_VERSION=.*$%POLYGLOT_RELEASE_VERSION=0.0.1%" polyglot-release
+
+popd > /dev/null
+
+
+# Make a release
+./../polyglot-release 1.0.0
