e39c90f5e72632ea0a31dcdc5015339eefd331ca
Aslak Hellesøy <1000+aslakhellesoy@users.noreply.github.com>
2022-05-23 18:44:24 +0100

    Fixes for MacOS. Add MacOS to CI matrix. (#74)

--- tests/created-by-contains-version.sh
+++ tests/created-by-contains-version.sh
@@ -7,8 +7,7 @@ pushd .. > /dev/null
 cp "$(which polyglot-release)" polyglot-release
 chmod 744 ./polyglot-release
 # Set the real script to a released version
-sed -i "s%^POLYGLOT_RELEASE_VERSION=.*$%POLYGLOT_RELEASE_VERSION=0.0.1%" polyglot-release
-
+sed -i".tmp" "s%^POLYGLOT_RELEASE_VERSION=.*$%POLYGLOT_RELEASE_VERSION=0.0.1%" polyglot-release
 popd > /dev/null
 
 
