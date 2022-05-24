de933eff33a9ea710307254333a1922bbf565fa3
M.P. Korstanje <mpkorstanje@users.noreply.github.com>
2022-05-24 23:36:07 +0200

    Implement self-updating with --update (#71)

---
+++ tests/do-update-already-up-to-date.sh
@@ -0,0 +1,13 @@
+# fixture: ruby
+set -e
+
+# Copy the polyglot-release script and set the version to v0.0.1.
+# This simulates a user who has installed a released version of polyglot-release.
+pushd .. > /dev/null
+cp "$(which polyglot-release)" polyglot-release
+chmod 744 ./polyglot-release
+sed -i".tmp" "s/^POLYGLOT_RELEASE_VERSION=.*$/POLYGLOT_RELEASE_VERSION=0.0.1/" polyglot-release
+popd > /dev/null
+
+# Update polyglot release without any new updates present
+./../polyglot-release --update
---
+++ tests/version.sh
@@ -0,0 +1,1 @@
+polyglot-release --version
